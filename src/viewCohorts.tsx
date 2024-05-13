de95901fd4ea4fa00495145ee7ca5cf929b5f6b8
Ashley <ashleyndabai@gmail.com>
2024-05-13 21:55:21 +0100

    correct view cohorts

--- src/viewCohorts.tsx
+++ src/viewCohorts.tsx
@@ -1,29 +1,3 @@
-// const viewCohorts = [
-//     { 
-//       cohortName: "Cohort 1",
-//       description: "John aces",
-//       program: "Java",
-//       startDate: new Date("2021-01-02"),
-//     },
-//     { 
-//         cohortName: "Cohort 2",
-//         program: "Java",
-//         startDate: new Date("2021-01-02"),
-//     },
-//     { 
-//         cohortName: "Cohort 3",
-//         program: "Java",
-//         startDate: new Date("2021-01-02"),
-//     },
-//     { 
-//         cohortName: "Cohort 4",
-//         program: "Java",
-//         startDate: new Date("2021-01-02"),
-//     },
-
-// ]
-
-// export default viewCohorts;
 const viewCohorts = 
     [{
         "cohortName": "Cohort 1",
@@ -48,26 +22,6 @@ const viewCohorts =
       }, {
         "cohortName": "Cohort 5",
         "program": "Software Engineering",
-        "startDate": "02-Jan-2021"
-      }, {
-        "cohortName": "Cohort 6",
-        "program": "Developers Associate",
-        "startDate": "02-Jan-2021"
-      }, {
-        "cohortName": "Cohort 7",
-        "program": "Developers Associate",
-        "startDate": "02-Jan-2021"
-      }, {
-        "cohortName": "Cohort 8",
-        "program": "Developers Associate",
-        "startDate": "02-Jan-2021"
-      }, {
-        "cohortName": "Cohort 9",
-        "program": "Developers Associate",
-        "startDate": "02-Jan-2021"
-      }, {
-        "cohortName": "Cohort 10",
-        "program": "Developers Associate",
         "startDate": "02-Jan-2021"
       }, 
     ]
